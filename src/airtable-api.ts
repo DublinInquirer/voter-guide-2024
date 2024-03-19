9212adc1400533b125c8e0ada7a9a3b7b5bd4ede
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-03-19 19:43:15 +0000

    Improve access to airtable fields

--- src/airtable-api.ts
+++ src/airtable-api.ts
@@ -1,2 +1,20 @@
-import airtable from 'airtable'
-export const airtableClient = new airtable({apiKey: import.meta.env.AIRTABLE_API_KEY}).base('appv26VnG13fZRP15');
+import airtable, { type Record, type FieldSet } from "airtable";
+import { z } from "zod";
+
+export const airtableClient = new airtable({
+  apiKey: import.meta.env.AIRTABLE_API_KEY,
+}).base("appv26VnG13fZRP15");
+
+export function getLookupFieldValue(
+  record: Record<FieldSet>,
+  fieldName: string
+) {
+  return z.array(z.string()).parse(record.get(fieldName))[0];
+}
+
+export function getStringFieldValue(
+  record: Record<FieldSet>,
+  fieldName: string
+) {
+  return z.string().parse(record.get(fieldName));
+}
