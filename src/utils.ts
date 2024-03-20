5fb0032d05d39fe1c30ee0f4f95ff7e97db2d561
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-03-20 00:09:10 +0000

    Add area selector to area page

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import type { Records, FieldSet } from "airtable";
+
+import { getLookupFieldValue, getStringFieldValue } from "./airtable-api";
+
 export function getCandidateFullName({
   firstname,
   lastname,
@@ -6,4 +10,30 @@ export function getCandidateFullName({
   lastname: string;
 }) {
   return `${firstname} ${lastname}`;
+}
+
+export function getAreasByLocalAuthority(electoralAreas: Records<FieldSet>) {
+  const dccElectoralAreaNames = electoralAreas
+    .filter((electoralArea) => {
+      const localAuthorityName = getLookupFieldValue(
+        electoralArea,
+        "Local Authority Name"
+      );
+
+      return localAuthorityName === "Dublin City Council";
+    })
+    .map((record) => getStringFieldValue(record, "Name"));
+
+  const fingalElectoralAreaNames = electoralAreas
+    .filter((electoralArea) => {
+      const localAuthorityName = getLookupFieldValue(
+        electoralArea,
+        "Local Authority Name"
+      );
+
+      return localAuthorityName === "Fingal County Council";
+    })
+    .map((record) => getStringFieldValue(record, "Name"));
+
+  return { dccElectoralAreaNames, fingalElectoralAreaNames };
 }
