b3d0dce1cf34e843f066dafd71b16c91922d1d83
Tewson Seeoun <tewson.seeoun@gmail.com>
2024-04-12 19:04:39 +0100

    Update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://dublininquirer.github.io",
-  base: "/voters-agenda-2024",
+  base: "/voter-guide-2024",
   build: {
     format: "file",
   },
